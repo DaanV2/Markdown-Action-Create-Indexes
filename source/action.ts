a9303d86f7d1e8f988e05d7e8d179c2e86f7173c
DaanV2 <daanverstraten@hotmail.com>
2022-07-09 11:48:34 +0200

    Updated action

--- source/action.ts
+++ source/action.ts
@@ -1,45 +1,41 @@
-import { CreateFolder } from './traverse';
-import * as fs from 'fs';
+import { CreateFolder } from "./traverse";
+import * as fs from "fs";
 
-const corexp = require('@actions/core');
+const corexp = require("@actions/core");
 
 //Start code
 try {
-	// This should be a token with access to your repository scoped in as a secret.
-	// The YML workflow will need to set myToken with the GitHub Secret Token
-	// token: ${{ secrets.GITHUB_TOKEN }}
-	const Folder = corexp.getInput('folder');
-	var result = false;
+  // This should be a token with access to your repository scoped in as a secret.
+  // The YML workflow will need to set myToken with the GitHub Secret Token
+  // token: ${{ secrets.GITHUB_TOKEN }}
+  const Folder = corexp.getInput("folder");
+  const filename = corexp.getInput("filename");
 
-	console.log('starting on: ' + Folder);
+  var result = false;
 
-	if (fs.existsSync(Folder)) {
-		result = CreateFolder(Folder);
-	} else {
-		throw { message: 'Couldnt not find folder: ' + Folder };
-	}
+  console.log("starting on: " + Folder);
 
-	if (result) {
-		console.log('success');
-	}
-	else {
-		console.log('failure');
-		corexp.setFailed('no pages were created');
-	}
+  if (fs.existsSync(Folder)) {
+    result = CreateFolder(Folder, filename);
+  } else {
+    throw { message: "Couldnt not find folder: " + Folder };
+  }
 
+  if (result) {
+    console.log("success");
+  } else {
+    console.log("failure");
+    corexp.setFailed("no pages were created");
+  }
 } catch (error) {
-	let message: string;
+  let message: string;
 
-	if (error.message)
-		message = error.message;
-	else
-		message = JSON.stringify(error);
+  if (error.message) message = error.message;
+  else message = JSON.stringify(error);
 
-	if (corexp)
-		corexp.setFailed(message);
-
-	else {
-		console.log(message);
-		process.exit(1);
-	}
+  if (corexp) corexp.setFailed(message);
+  else {
+    console.log(message);
+    process.exit(1);
+  }
 }
--- source/traverse.ts
+++ source/traverse.ts
@@ -1,5 +1,5 @@
-import * as fs from 'fs';
-import path from 'path';
+import * as fs from "fs";
+import path from "path";
 
 const Template = `# {$HEADER$}
 
@@ -10,18 +10,16 @@ const Template = `# {$HEADER$}
 {$DOCUMENTS$}`;
 
 const WriteOptions: fs.WriteFileOptions = {
-  encoding: 'utf8'
-}
-
+  encoding: "utf8",
+};
 
 /**
  * Creates a markdown index page
- * @param folder 
+ * @param folder
  * @returns True if the page was made.
  */
-export function CreateFolder(folder: string): boolean {
-  if (folder.includes('.git'))
-    return false;
+export function CreateFolder(folder: string, filename: string): boolean {
+  if (folder.includes(".git")) return false;
 
   let SubFolders: string[] = [];
   let Documents: string[] = [];
@@ -41,13 +39,12 @@ export function CreateFolder(folder: string): boolean {
       if (fs.statSync(subfolder).isDirectory()) {
         //Create index page, if succesfull we create a reference
 
-        if (CreateFolder(subfolder)) {
-          SubFolders.push(`- [${child}](./${encodeURI(child)}/index.md)`);
+        if (CreateFolder(subfolder, filename)) {
+          SubFolders.push(`- [${child}](./${encodeURI(child)}/${filename})`);
         }
-      }
-      else {
+      } else {
         //If the child is a .md page create a reference
-        if (child.endsWith('.md') && child != 'index.md') {
+        if (child.endsWith(".md") && child != filename) {
           Documents.push(`- [${child.substring(0, child.length - 3)}](${encodeURI(child)})`);
         }
       }
@@ -56,13 +53,13 @@ export function CreateFolder(folder: string): boolean {
 
   //If there are any reference made we create the index page and return succes
   if (SubFolders.length > 0 || Documents.length > 0) {
-    let filepath = path.join(folder, 'index.md');
+    let filepath = path.join(folder, filename);
     let Name = GetFolderName(folder);
-    console.log('writing: ' + filepath);
+    console.log("writing: " + filepath);
 
     let Content = Template.replace(/\{\$HEADER\$\}/gi, Name);
-    Content = Content.replace(/\{\$CATEGORIES\$\}/gi, SubFolders.join('\r\n'));
-    Content = Content.replace(/\{\$DOCUMENTS\$\}/gi, Documents.join('\r\n'));
+    Content = Content.replace(/\{\$CATEGORIES\$\}/gi, SubFolders.join("\r\n"));
+    Content = Content.replace(/\{\$DOCUMENTS\$\}/gi, Documents.join("\r\n"));
 
     fs.writeFileSync(filepath, Content, WriteOptions);
     return true;
@@ -71,14 +68,13 @@ export function CreateFolder(folder: string): boolean {
   return false;
 }
 
-
 /**
  * Returns the name of the folder
  * @param folderpath The whole folder path
  * @returns The name of the folder
  */
 function GetFolderName(folderpath: string): string {
-  let LastIndex = folderpath.lastIndexOf('/');
+  let LastIndex = folderpath.lastIndexOf("/");
 
   if (LastIndex >= 0) {
     return folderpath.substring(LastIndex + 1, folderpath.length);
